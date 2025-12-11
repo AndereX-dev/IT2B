bdd8e0e71211c595ffbd056ca44f0686117f6033
AndereX-dev <andreas.osdal@hotmail.com>
2025-12-11 19:29:04 +0100

    commit

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
-const mysql = require("mysql2/promise");
+const mySql = require("mysql2/promise");
 
 async function createConnection() {
-  return (connection = await mysql.createConnection({
+  return (connection = await mySql.createConnection({
     host: "localhost",
     user: "root",
     password: "Youcant-Be-Here25",
